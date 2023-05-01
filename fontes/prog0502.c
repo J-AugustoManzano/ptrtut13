5daacd1f3018fe9986f3b3e51d018b4733fc7267
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-05-01 10:58:05 -0300

    Update prog0502.c

--- fontes/prog0502.c
+++ fontes/prog0502.c
@@ -1,4 +1,4 @@
-/* Programa 5.2 extraído de PTRTUT10.TXT - 13/06/1997 */
+/* Programa 5.2 extraido de PTRTUT10.TXT - 13/06/1997 */
 /*              adaptado por AM-42       - 28/04/2023 */
 /*                                 youtube.com/@AM-42 */
 
